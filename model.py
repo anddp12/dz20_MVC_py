f2f1c4cfdd31923abf41fde89d1d9760a61b01fc
anddp12 <anddp12@gmail.com>
2022-12-28 22:25:01 +0200

    creation of methods cube, check, score (in model)

---
+++ model.py
@@ -0,0 +1,18 @@
+from random import randint
+
+def cube(end=6):
+    return randint(1, end)
+
+def score(score, cube):
+    if cube == 6:
+        score += 3
+        return score
+    elif cube != 6:  
+        score -= 1
+        return score
+
+def check(score):
+    if score < 0:
+        return 'Вы проиграли', False
+    elif score > 0:
+        return 'Вы еще в игре', True
